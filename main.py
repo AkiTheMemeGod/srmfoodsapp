845194a192d7c148734c0dbd9e49273eeff04612
AkiTheMemeGod <k.akashkumar@gmail.com>
2024-02-14 21:59:54 +0530

    testing out container and themes

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ st.markdown("""
 custom_title("l", 50, "#253d44", "SRM FOODS<br><br>")
 db = Database()
 foods = db.get_data("nonveg")
-left, c1, mid, c2, right = st.columns([1,2, 0.25, 2, 1])
+left, c1, mid, c2, right = st.columns([1, 2, 0.25, 2, 1])
 with c1:
     for i in foods[:2]:
         with st.container(height=540, border=True):
@@ -34,4 +34,6 @@ with c2:
             st.image(i[2], width=400)
             st.header(f"Price : {i[3]}")
         st.button("Add to Cart", key=i[0], type="primary", use_container_width=True)
+        st.markdown("###")
+
 

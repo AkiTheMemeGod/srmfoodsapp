8d45e62c099b1ae757079d56f5f604089a43a1c4
AkiTheMemeGod <k.akashkumar@gmail.com>
2024-02-14 20:11:39 +0530

    testing out container and themes

--- dependencies.py
+++ dependencies.py
@@ -10,10 +10,21 @@ class Database:
 
         cursor = self.connection.cursor()
         cursor.execute(f"SELECT * FROM {food}")
-        data = cursor.fetchall()[0][4]
-        print(data)
+        data = cursor.fetchall()
+        # print(data)
+        return data
 
 
-db = Database()
-db.get_data("nonveg")
+def custom_title(pos, size, color, title, align="center"):
+    if pos == "side":
+        st.sidebar.markdown(f"""
+                        <h1 style="font-family:monospace; color:{color}; font-size: {size}px;", align="{align}">{title}</h1>
+                        """,
+                            unsafe_allow_html=True)
+
+    else:
+        st.markdown(f"""
+                <h1 style="font-family:monospace; color:{color}; font-size: {size}px;", align="{align}">{title}</h1>
+""",
+                    unsafe_allow_html=True)
 
--- main.py
+++ main.py
@@ -1,5 +1,37 @@
-import streamlit as st
+from dependencies import *
 
-st.set_page_config(page_title="SRM-Goodfoods", page_icon="🍔")
+st.set_page_config(page_title="SRM-Goodfoods", page_icon="🍔", layout="wide")
 
-st.title("SRM FOODS")
+st.markdown("""
+            <style>
+            .st-emotion-cache-1v0mbdj > img{
+                border-radius: 6.1%;
+
+                }
+            </style>
+
+            """, unsafe_allow_html=True)
+
+custom_title("l", 50, "#253d44", "SRM FOODS<br><br>")
+db = Database()
+foods = db.get_data("nonveg")
+left, c1, mid, c2, right = st.columns([1,2, 0.25, 2, 1])
+with c1:
+    for i in foods[:2]:
+        with st.container(height=540, border=True):
+            custom_title("l", 45, "#253d44", f"{i[0]}")
+            st.subheader(i[1])
+            st.image(i[2], width=400)
+            st.header(f"Price : {i[3]}")
+        st.button("Add to Cart", key=i[0], type="primary", use_container_width=True)
+        st.markdown("###")
+
+with c2:
+    for i in foods[2:]:
+        with st.container(height=540, border=True):
+            custom_title("l", 45, "#253d44", f"{i[0]}")
+            st.subheader(i[1])
+            st.image(i[2], width=400)
+            st.header(f"Price : {i[3]}")
+        st.button("Add to Cart", key=i[0], type="primary", use_container_width=True)
+

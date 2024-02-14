954706214b65b85429938b7951c56e8be26f127f
AkiTheMemeGod <k.akashkumar@gmail.com>
2024-02-14 18:21:47 +0530

    minor changes

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import streamlit as st
-# import streamlit_shadcn_ui as ui
 
-st.set_page_config(page_title="SRM-Goodfoods", page_icon="🍔")#title
+st.set_page_config(page_title="SRM-Goodfoods", page_icon="🍔")
 
